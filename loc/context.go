3c6eded6ce023c268db88fc56592041c96b3f4df
Igor Afanasyev <igor.afanasyev@gmail.com>
2017-10-02 18:28:39 -0700

    context: add GetLanguage() and GetContext() helpers.
    This allows one to manipulate contexts easily:
    1) change the language on the fly within the same
    localization pool.
    2) create a context for another localization pool with the same language as in the existing context.

--- loc/context.go
+++ loc/context.go
@@ -13,6 +13,20 @@ type Context struct {
 	plurr *plurr.Plurr
 }
 
+// GetLanguage returns the current language set for the context.
+func (lc *Context) GetLanguage() string {
+	return lc.lang
+}
+
+// GetContext returns a context for another language
+// withing the same localization pool. This can be useful to
+// switch the language within the same pool (think of it
+// as of a `SetLanguage()` function which returns a new context
+// without changing the previous one)
+func (lc *Context) GetContext(lang string) *Context {
+	return lc.pool.GetContext(lang)
+}
+
 // Tr returns a translated version of the string
 func (lc *Context) Tr(key string) string {
 	// try preferred language
